8de4f28355f81963b663a0000e7971ea67c0d698
Alex <alexandre.coetzee@gmail.com>
2019-06-30 13:04:13 +0200

    Fleshed out the accounts endpoints

--- 24hr-deals-api/models/products.model.js
+++ 24hr-deals-api/models/products.model.js
@@ -37,6 +37,7 @@ productsModel.getProduct = (id) => {
 productsModel.updateProduct = (product) => {
     let productIndex = productsModel.products.findIndex(x => x.id === product.id);
     if (productIndex === -1) {
+        // If we can't find the product we create one
         this.addProduct(product)
     } else {
         this.products[product.id] = product;
@@ -45,9 +46,6 @@ productsModel.updateProduct = (product) => {
 
 productsModel.removeProduct = (id) => {
     let productIndex = productsModel.products.findIndex(x => x.id === id);
-    let product = productsModel.products.find(x => x.id === id);
-    console.log(product);
-    console.log(productIndex);
 
     if (productIndex !== -1) {
         productsModel.products.splice(productIndex, 1);
