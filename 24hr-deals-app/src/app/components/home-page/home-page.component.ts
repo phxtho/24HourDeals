a3f874171ef575e5dc0d233557f734decca15159
mengess <storm.menges@vcontractor.co.za>
2019-07-04 17:40:10 +0200

    Reverted

--- 24hr-deals-app/src/app/components/home-page/home-page.component.ts
+++ 24hr-deals-app/src/app/components/home-page/home-page.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductModel } from 'src/app/models/product/product-model';
-import { Subscription } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { ProductService } from 'src/app/services/product.service';
+import { Component, OnInit } from "@angular/core";
+import { ProductModel } from "src/app/models/product/product-model";
+import { Subscription } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { ProductService } from "src/app/services/product.service";
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  selector: "app-home-page",
+  templateUrl: "./home-page.component.html",
+  styleUrls: ["./home-page.component.scss"]
 })
 export class LandingPageComponent implements OnInit {
-
   private products: ProductModel[];
   date: Date;
   private subscription = new Subscription();
